795c0110d5fc819922ef6c4a5c1f9c392a2efcca
benoit tuduri <benoit.tuduri@gmail.com>
2014-09-13 20:11:08 +0200

    if vote/upload prod is closed then homepage else if vote only a page invite the user to vote else you can fill out a new prod. the production updated is managed.

--- app/controllers/ProductionCtl.java
+++ app/controllers/ProductionCtl.java
@@ -22,10 +22,19 @@ public class ProductionCtl extends Controller {
     static Form<Production> formulaire = form(Production.class);
 
     public static Result index() {
+        List<Compo> l = Compo.find.where().eq("uploadOpen", true).findList();
+        if (l != null && l.size() > 0) {
         return ok(
             listeProductionUploadView.render(
                 Production.find.where()
                     .orderBy("name ASC").findList()));
+        } else {
+            l = Compo.find.where().eq("voteOpen", true).findList();
+            if (l != null && l.size() > 0) {
+                return ok(gotoVoteProductionUploadView.render());
+            }
+            return redirect("/");
+        }
     }
 
     public static Result show() {
@@ -34,12 +43,41 @@ public class ProductionCtl extends Controller {
 
     public static Result upload() {
         Form<Production> filledForm = formulaire.bindFromRequest();
+        Logger.info(" filledForm created ");
+        Logger.info("compo = " + filledForm.field("compo").value());
+        Logger.info("name = " + filledForm.field("name").value());
+        Logger.info("comment = " + filledForm.field("comment").value());
+        Logger.info("user = " + filledForm.field("user").value());
+
+        if (filledForm.hasErrors()) {
+           Logger.info("I ve some errors");
+            Logger.error(filledForm.error("compo").message());
+           return badRequest(
+                creationProductionUploadView.render(filledForm)
+            );
+       }
+
+        Production p = Production.find.where().eq("user",
+            filledForm.field("user").value())
+                .eq("compo",
+                    filledForm.field("compo").value()).findUnique();
+
+        if (p != null) {
+            try {
+                Logger.debug("---> suppression de l'ancien fichier " + Paths.get("/tmp", p.filename));
+                Files.delete(Paths.get("/tmp", p.filename));
+            } catch (IOException ioe) {
+                Logger.debug(ioe.getMessage());
+            }
+        }
+
         MultipartFormData body = request().body().asMultipartFormData();
         FilePart prod = body.getFile("filename");
+        Logger.info("filename uploaded = " + prod.getFilename());
 
         if (prod == null) {
                 Logger.debug("probleme upload fichier");
-                filledForm.reject("filename", "fichier invalide");
+                filledForm.reject("filename", "fichier invalide ou non specifié");
                 return badRequest(
                     creationProductionUploadView.render(filledForm)
                 );
@@ -51,23 +89,26 @@ public class ProductionCtl extends Controller {
             Logger.debug(ioe.getMessage());
         }
 
-        Logger.info(" filledForm created ");
-        Logger.info("compo = " + filledForm.field("compo").value());
-        Logger.info("name = " + filledForm.field("name").value());
-        Logger.info("comment = " + filledForm.field("comment").value());
-        Logger.info("filename uploaded = " + prod.getFilename());
-        Logger.info("user = " + filledForm.field("user").value());
 
-        if (filledForm.hasErrors()) {
-           Logger.info("I ve some errors");
-            Logger.error(filledForm.error("compo").message());
-           return badRequest(
-                creationProductionUploadView.render(filledForm)
-            );
-       }
+        if (p == null) {
+            p =filledForm.get();
+:q
+:q
+:q
 
-        Production p = filledForm.get();
-        p.filename = prod.getFilename();
+
+
+
+:q
+
+
+            p.filename = prod.getFilename();
+        } else {
+            p.name = filledForm.field("name").value();
+            p.comment = filledForm.field("comment").value();
+            p.filename = prod.getFilename();
+        }
+
         p.save();
         return GO_HOME;
     }
